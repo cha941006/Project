2df03d182bb814bda495576b330013566b58f42a
ChaSeongMin <cha941006@naver.com>
2023-01-14 23:15:58 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import './App.css';
 import Customer from './components/Customer';
 import { render } from '@testing-library/react';
 import { createContext } from 'react';
+import TableHead from '@mui/material/TableHead';
+import Table from '@mui/material/Table';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import { TableBody } from '@mui/material';
+import { withStyles}  from '@mui/material';
+import Paper from '@mui/material/Paper';
+
+const styles = theme =>({
+  root:{
+    width : '100%',
+    marginTop : theme.spacing.unit *3,
+    overflowX : "auto"
+  },
+  table:{
+    minWidth : 1080
+  }
+
+})
 
 
 const customers =[
@@ -33,28 +52,27 @@ const customers =[
 ]
 
 function App() {
+  const {classes} = this.props;
   return (
-    <div>
-      {
-        customers.map(c =>{
-          return(
-            <Customer
-            key = {c.id}
-            id ={c.id}
-            image = {c.image}
-            name = {c.name}
-            birthday = {c.birthday}
-            gender = {c.gender}
-            job = {c.job}
+    <Paper className = {classes.root}>    
+      <Table className = {classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
 
-            />
-
-          )
-        })
-      }
-
-    </div>
-    )
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map(c =>{ return(<Customer key = {c.id} id ={c.id} image = {c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job}/>)})}   
+        </TableBody>
+      </Table>
+    </Paper>
+  )
 }
 
-export default App;
+export default withStyles(styles)(App);
